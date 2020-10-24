807d468d17f8b99e8fd29a2135b47b22252c5d9b
unknown <you@example.com>
2020-10-24 15:33:49 -0300

    primeiro controller e teste paginas Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "carlos@gmail.com";
 
             return View();
         }
